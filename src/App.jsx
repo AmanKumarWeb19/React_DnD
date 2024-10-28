9a603d5a1e19a943aba10f622a833e296dcbc6fc
Aman Kumar <akgupta0524@gmail.com>
2024-10-28 13:41:27 +0530

    complete the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import CreateTask from "./components/CreateTask";
 import ListTasks from "./components/ListTasks";
 import { Toaster } from "react-hot-toast";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -13,13 +15,16 @@ function App() {
   }, []);
 
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Toaster />
-      <div className="bg-slate-200 w-screen h-screen flex flex-col items-center pt-32 gap-16">
+      <div className="bg-slate-200 w-screen h-screen flex flex-col items-center p-3 pt-32 gap-16">
+        <h1 className="bg bg-red-400 w-64 h-12 flex justify-center items-center rounded-md text-4xl text-red-800">
+          Todo DnD
+        </h1>
         <CreateTask tasks={tasks} setTasks={setTasks} />
         <ListTasks tasks={tasks} setTasks={setTasks} />
       </div>
-    </>
+    </DndProvider>
   );
 }
 
--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -1,7 +1,169 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import toast from "react-hot-toast";
 
 const ListTasks = ({ tasks, setTasks }) => {
-  return <div>ListTasks</div>;
+  const [todos, setTodos] = useState([]);
+  const [inProgress, setInProgress] = useState([]);
+  const [complete, setComplete] = useState([]);
+
+  useEffect(() => {
+    const fTodos = tasks.filter((task) => task.status === "todo");
+    const fInProgress = tasks.filter((task) => task.status === "inprogress");
+    const fComplete = tasks.filter((task) => task.status === "complete");
+
+    setTodos(fTodos);
+    setInProgress(fInProgress);
+    setComplete(fComplete);
+  }, [tasks]);
+
+  const statuses = ["todos", "inprogress", "complete"];
+
+  return (
+    <div className="flex gap-16">
+      {statuses.map((status, index) => (
+        <Section
+          key={index}
+          status={status}
+          tasks={tasks}
+          setTasks={setTasks}
+          todos={todos}
+          inProgress={inProgress}
+          complete={complete}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ListTasks;
+
+//............................................Section...........................................................
+
+const Section = ({ status, tasks, setTasks, todos, inProgress, complete }) => {
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "task",
+    drop: (item) => addItemToSection(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  let text = "Todo";
+  let bg = "bg-slate-500";
+  let tasksToMap = todos;
+
+  if (status === "inprogress") {
+    text = "In Progress";
+    bg = "bg-purple-500";
+    tasksToMap = inProgress;
+  }
+
+  if (status === "complete") {
+    text = "Complete";
+    bg = "bg-green-500";
+    tasksToMap = complete;
+  }
+
+  const addItemToSection = (id) => {
+    setTasks((prev) => {
+      const mTask = prev.map((t) => {
+        if (t.id === id) {
+          return { ...t, status: status };
+        }
+        return t;
+      });
+
+      localStorage.setItem("tasks", JSON.stringify(mTask));
+
+      toast("Task Status Changed", { icon: "✔️" });
+
+      return mTask;
+    });
+  };
+
+  return (
+    <div
+      ref={drop}
+      className={`w-64 rounded-md p-2 ${isOver ? "bg-slate-300" : ""}`}
+    >
+      <Header text={text} bg={bg} count={tasksToMap.length} />
+      {tasksToMap.length > 0 &&
+        tasksToMap.map((task) => (
+          <SingleTask
+            key={task.id}
+            task={task}
+            tasks={tasks}
+            setTasks={setTasks}
+          />
+        ))}
+    </div>
+  );
+};
+
+//..........................................Header........................................................
+
+const Header = ({ text, bg, count }) => {
+  return (
+    <div
+      className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white`}
+    >
+      {text}
+      <div className="ml-2 bg-white w-5 h-5 text-black rounded-full flex items-center justify-center">
+        {count}
+      </div>
+    </div>
+  );
+};
+
+//................................................Single Task...............................................
+
+const SingleTask = ({ task, tasks, setTasks }) => {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: "task",
+    item: { id: task.id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  const handleRemove = (id) => {
+    console.log(id);
+
+    const fTasks = tasks.filter((t) => t.id !== id);
+    localStorage.setItem("tasks", JSON.stringify(fTasks));
+    setTasks(fTasks);
+
+    toast("Task Removed", { icon: "💀" });
+  };
+
+  return (
+    <div
+      ref={drag}
+      className={`relative p-4 mt-8 rounded-md shadow-md cursor-grab ${
+        isDragging ? "opacity-25" : "opacity-100"
+      }`}
+    >
+      <p>{task.name}</p>
+      <button
+        className="absolute bottom-1 right-1 text-slate-400"
+        onClick={() => handleRemove(task.id)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
