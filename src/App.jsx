ab9050d11fb8c1ee143a4b3f0626a7f7ba804205
Aman Kumar <akgupta0524@gmail.com>
2024-10-28 09:40:00 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  return (
+    <>
+      <div className="text-orange-500">Hello world!</div>
+    </>
+  );
+}
+
+export default App;
