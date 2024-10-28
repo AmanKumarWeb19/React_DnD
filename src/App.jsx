2f98d7a7c347f024098f13c86099cc224c1df77e
Aman Kumar <akgupta0524@gmail.com>
2024-10-28 11:10:58 +0530

    using react toast in create component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateTask from "./components/CreateTask";
 import ListTasks from "./components/ListTasks";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
+  console.log("Tasks :-", tasks);
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("tasks")));
+  }, []);
+
   return (
     <>
-      <div className="bg-slate-200 w-screen h-screen flex flex-col items-center pt-3 gap-16">
+      <Toaster />
+      <div className="bg-slate-200 w-screen h-screen flex flex-col items-center pt-32 gap-16">
         <CreateTask tasks={tasks} setTasks={setTasks} />
         <ListTasks tasks={tasks} setTasks={setTasks} />
       </div>
